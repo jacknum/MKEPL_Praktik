8b7966dd2747e19f13790b0896f3ddaca8f761bb
jacknum <syahrulzaki0706@gmail.com>
2025-04-21 14:49:02 +0700

    refactoring: mengekstrak data membership dan logic ke dalam inner class Keanggotaan

--- 08_Assessment_Tugas_Besar_Pengganti_UTS/LibraryMember.java
+++ 08_Assessment_Tugas_Besar_Pengganti_UTS/LibraryMember.java
@@ -3,15 +3,9 @@ import java.util.Calendar;
 
 public class LibraryMember {
 
-    private String namaLengkap;
-    private String alamat;
-    private String nomorTelepon;
-    private String email;
-    private String jenisKelamin;
-    private String kodeAnggota;
-    private Date tanggalGabung;
+    private MemberInfo info;
     private boolean statusAktif;
-    private String tingkatKeanggotaan;
+    private Keanggotaan Keanggotaan;
     private int jumlahBukuDipinjam;
     private int jumlahTerlambat;
     private int jumlahDenda;
@@ -19,27 +13,96 @@ public class LibraryMember {
     private String kodeReferal;
     private boolean langgananBuletin;
 
-    public LibraryMember(String namaLengkap, String alamat, String nomorTelepon, String email, String jenisKelamin,
-            String kodeAnggota, Date tanggalGabung, boolean statusAktif,
+    public LibraryMember(MemberInfo info, boolean statusAktif,
             String tingkatKeanggotaan, int jumlahBukuDipinjam,
-            int jumlahTerlambat, int jumlahDenda, int poinLoyalitas,
-            String kodeReferal, boolean langgananBuletin) {
+            int jumlahTerlambat, int jumlahDenda,
+            int poinLoyalitas, String kodeReferal,
+            boolean langgananBuletin) {
 
-        this.namaLengkap = namaLengkap;
-        this.alamat = alamat;
-        this.nomorTelepon = nomorTelepon;
-        this.email = email;
-        this.jenisKelamin = jenisKelamin;
-        this.kodeAnggota = kodeAnggota;
-        this.tanggalGabung = tanggalGabung;
+        this.info = info;
         this.statusAktif = statusAktif;
-        this.tingkatKeanggotaan = tingkatKeanggotaan;
+        this.Keanggotaan = new Keanggotaan(tingkatKeanggotaan, poinLoyalitas);
         this.jumlahBukuDipinjam = jumlahBukuDipinjam;
         this.jumlahTerlambat = jumlahTerlambat;
         this.jumlahDenda = jumlahDenda;
         this.poinLoyalitas = poinLoyalitas;
         this.kodeReferal = kodeReferal;
         this.langgananBuletin = langgananBuletin;
+    }
+
+    public void cetakProfilLengkap() {
+        System.out.println("===== PROFIL ANGGOTA =====");
+        System.out.println("Nama         : " + info.getNamaLengkap());
+        System.out.println("Jenis Kelamin: " + info.getJenisKelamin());
+        System.out.println("Alamat       : " + info.getAlamat());
+        System.out.println("Telepon      : " + info.getNomorTelepon());
+        System.out.println("Email        : " + info.getEmail());
+        System.out.println("Kode Anggota : " + info.getKodeAnggota());
+        System.out.println("Tanggal Gabung: " + info.getTanggalGabung());
+        System.out.println("Status Aktif : " + statusAktif);
+        System.out.println("Tingkat      : " + Keanggotaan.getTingkat());
+        System.out.println("Buku Dipinjam: " + jumlahBukuDipinjam);
+        System.out.println("Terlambat    : " + jumlahTerlambat);
+        System.out.println("Denda        : Rp " + jumlahDenda);
+        System.out.println("Poin         : " + poinLoyalitas);
+        System.out.println("Kode Referal : " + kodeReferal);
+        System.out.println("Langganan Buletin: " + langgananBuletin);
+        System.out.println("Skor Risiko : " + hitungSkorRisiko());
+        System.out.println("Layak Upgrade?: " + Keanggotaan.layakUpgrade());
+        System.out.println("===========================");
+    }
+
+    public double hitungSkorRisiko() {
+        double skor = 0;
+        skor += jumlahTerlambat * 1.5;
+        skor += jumlahDenda * 0.1;
+        if (!statusAktif)
+            skor += 5;
+        if (Keanggotaan.equals("DASAR"))
+            skor += 2;
+        if (jumlahBukuDipinjam > 50)
+            skor -= 1.5;
+        return skor;
+    }
+
+    public int getJumlahBukuDipinjam() {
+        return jumlahBukuDipinjam;
+    }
+
+    public void setJumlahBukuDipinjam(int b) {
+        jumlahBukuDipinjam = b;
+    }
+
+    public int getJumlahTerlambat() {
+        return jumlahTerlambat;
+    }
+
+    public void setJumlahTerlambat(int t) {
+        jumlahTerlambat = t;
+    }
+
+    public int getJumlahDenda() {
+        return jumlahDenda;
+    }
+
+    public void setJumlahDenda(int d) {
+        jumlahDenda = d;
+    }
+
+    public String getKodeReferal() {
+        return kodeReferal;
+    }
+
+    public void setKodeReferal(String k) {
+        kodeReferal = k;
+    }
+
+    public boolean getLanggananBuletin() {
+        return langgananBuletin;
+    }
+
+    public void setLanggananBuletin(boolean l) {
+        langgananBuletin = l;
     }
 
     public static class MemberInfo {
@@ -91,93 +154,33 @@ public class LibraryMember {
         }
     }
 
-    public void cetakProfilLengkap() {
-        System.out.println("===== PROFIL ANGGOTA =====");
-        System.out.println("Nama         : " + namaLengkap);
-        System.out.println("Jenis Kelamin: " + jenisKelamin);
-        System.out.println("Alamat       : " + alamat);
-        System.out.println("Telepon      : " + nomorTelepon);
-        System.out.println("Email        : " + email);
-        System.out.println("Kode Anggota : " + kodeAnggota);
-        System.out.println("Tanggal Gabung: " + tanggalGabung);
-        System.out.println("Status Aktif : " + statusAktif);
-        System.out.println("Tingkat      : " + tingkatKeanggotaan);
-        System.out.println("Buku Dipinjam: " + jumlahBukuDipinjam);
-        System.out.println("Terlambat    : " + jumlahTerlambat);
-        System.out.println("Denda        : Rp " + jumlahDenda);
-        System.out.println("Poin         : " + poinLoyalitas);
-        System.out.println("Kode Referal : " + kodeReferal);
-        System.out.println("Langganan Buletin: " + langgananBuletin);
-        System.out.println("Skor Risiko : " + hitungSkorRisiko());
-        System.out.println("Layak Upgrade?: " + periksaKelayakanUpgrade());
-        System.out.println("===========================");
-    }
+    public static class Keanggotaan {
+        private String tingkat;
+        private int poin;
 
-    public boolean periksaKelayakanUpgrade() {
-        if (tingkatKeanggotaan.equals("DASAR") && poinLoyalitas > 100) {
-            return true;
+        public Keanggotaan(String tingkat, int poin) {
+            this.tingkat = tingkat;
+            this.poin = poin;
         }
-        return false;
-    }
 
-    public double hitungSkorRisiko() {
-        double skor = 0;
-        skor += jumlahTerlambat * 1.5;
-        skor += jumlahDenda * 0.1;
-        if (!statusAktif)
-            skor += 5;
-        if (tingkatKeanggotaan.equals("DASAR"))
-            skor += 2;
-        if (jumlahBukuDipinjam > 50)
-            skor -= 1.5;
-        return skor;
-    }
+        public boolean layakUpgrade() {
+            return tingkat.equalsIgnoreCase("DASAR") && poin > 100;
+        }
 
-    public int getJumlahBukuDipinjam() {
-        return jumlahBukuDipinjam;
-    }
+        public String getTingkat() {
+            return tingkat;
+        }
 
-    public void setJumlahBukuDipinjam(int b) {
-        jumlahBukuDipinjam = b;
-    }
+        public int getPoin() {
+            return poin;
+        }
 
-    public int getJumlahTerlambat() {
-        return jumlahTerlambat;
-    }
+        public void setTingkat(String tingkat) {
+            this.tingkat = tingkat;
+        }
 
-    public void setJumlahTerlambat(int t) {
-        jumlahTerlambat = t;
-    }
-
-    public int getJumlahDenda() {
-        return jumlahDenda;
-    }
-
-    public void setJumlahDenda(int d) {
-        jumlahDenda = d;
-    }
-
-    public String getTingkatKeanggotaan() {
-        return tingkatKeanggotaan;
-    }
-
-    public void setTingkatKeanggotaan(String t) {
-        tingkatKeanggotaan = t;
-    }
-
-    public String getKodeReferal() {
-        return kodeReferal;
-    }
-
-    public void setKodeReferal(String k) {
-        kodeReferal = k;
-    }
-
-    public boolean getLanggananBuletin() {
-        return langgananBuletin;
-    }
-
-    public void setLanggananBuletin(boolean l) {
-        langgananBuletin = l;
+        public void setPoin(int poin) {
+            this.poin = poin;
+        }
     }
 }
